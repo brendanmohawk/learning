e4707c445f3e2776a0aa5d74a8b7dddf098b8b65
Brendan Dileo <dileob23@gmail.com>
2025-08-07 19:19:07 -0400

    Add inner iterator class

--- java-review/src/main/java/brendanddev/collections/list/CustomList.java
+++ java-review/src/main/java/brendanddev/collections/list/CustomList.java
@@ -4,6 +4,15 @@ import java.util.Iterator;
 
 /**
  * 
+ * WHY ANONYMOUS CLASS IS USED HERE:
+     * - Iterator is only needed internally by this CustomList
+     * - No reusability outside this context
+     * - Keeps related code together (iterator logic stays with the collection)
+     * - Cleaner than creating a separate IteratorImpl.java file
+     * - Has implicit access to outer class members (closure over CustomList state)
+
+
+ * 
  */
 public class CustomList<E> implements Iterable<E> {
 
@@ -36,8 +45,60 @@ public class CustomList<E> implements Iterable<E> {
         elements = (E[]) new Object[initialCapacity];
     }
 
-
+    /**
+     * Declares a anonymous inner class that implements the Iterator<E> interface.
+     * The anonymous class appears as if it has a method body since the method iterator() returns an instance
+     * of the interface. Instead of returning a named class instance, it returns a new instance of an anonymous class
+     * that implements the Iterator<E> interface inline. This creates a one off implementation of the interface without 
+     * needing to write a named class.
+     * 
+     * Since CustomList implements the Iterable interface (Iterable<E>), it must provide an implementation
+     * of the iterator() method. This method returns an Iterator<E> that can be used to traverse the elements
+     * of the CustomList without exposing its internal structure.
+     */
     @Override
-    public Iterator<E> iterator() { }
+    public Iterator<E> iterator() { 
+
+        // The actual anonymous inner class implementation starts here
+        // This 'new Iterator<E>() { ... }' syntax creates an unnamed class that
+        // implements Iterator<E> and returns an instance of it immediately
+        return new Iterator<E>() {
+
+            // Current position in the iteration
+            // Each iterator instance has its own cursor, allowing multiple iterators
+            // to traverse the same CustomList simultaneously without interfering
+            int cursor = 0;
+
+            /**
+             * Checks if there are more elements to iterate over.
+             * This method is called before each call to next() to avoid going
+             * beyond the collection bounds.
+             * 
+             * @return true if there are more elements, false if we've reached the end
+             */
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            /**
+             * Returns the next element in the iteration and advances the cursor.
+             * This method should only be called if hasNext() returns true.
+             * 
+             * @return the next element of type E
+             * @throws IndexOutOfBoundsException if there are no more elements
+             */
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException("No more elements to iterate over");
+                }
+                return elements[cursor++];
+            }
+        };
+    }
+
+
+
     
 }
