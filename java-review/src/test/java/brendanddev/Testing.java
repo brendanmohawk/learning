88185030994bbb161ac6c2e41edc7211ff4443b6
Brendan Dileo <dileob23@gmail.com>
2025-08-06 20:03:18 -0400

    Add basic test

--- java-review/src/test/java/brendanddev/Testing.java
+++ java-review/src/test/java/brendanddev/Testing.java
@@ -16,7 +16,15 @@ public class Testing {
     @Test 
     public void testAdd() {
         Calculator calculator = new Calculator();
+
+        // Call the the add method
         int result = calculator.add(2, 3);
+
+        System.out.println("Running testAdd: result = " + result);
+        
+        // Asser that the result should be 5
+        // If the assertion fails, it will throw an AssertionError
+        // with the message "Expected 5 but got <result>"
         assert result == 5 : "Expected 5 but got " + result;
     }
 
