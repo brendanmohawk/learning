5e142565c1252d77c18e166498a1c6b1753e0524
Brendan Dileo <dileob23@gmail.com>
2025-08-08 17:59:35 -0400

    Update groovy basics syntax review

--- groovy-review/src/main/groovy/brendan/groovyreview/basics/Syntax.groovy
+++ groovy-review/src/main/groovy/brendan/groovyreview/basics/Syntax.groovy
@@ -11,9 +11,13 @@ class Syntax {
     static void main(String[] args) {
 
         variableDelcarations()
+        elvisOperator()
     }
 
 
+    /**
+     * This method demonstrates 
+     */
     static void variableDelcarations() {
         // Can use def identifier for dynamic typing
         def name = "Brendan"
@@ -30,19 +34,64 @@ class Syntax {
         println "Name: $name, Age: $age, Learning: $isLearning, Nothing: $nothing"
     }
 
+    /**
+     * This method demonstrates the use of the safe navigation operator (?.) in groovy.
+     * This operator allows you to safely access properties or methods of an object that might be null
+     * without throwing a NullPointerException.
+     */
     static void elvisOperator() {
+
+        def nullableValue = null
+
+        // Uses the Elvis operator '?:' to provide a default value if the left side is null
+        // If left side is not null, it returns that value
+        def result = nullableValue ?: "Default Value"
+        println "null ?: 'default': $result"
+
+        // Since value is not null, it returns the actual value
+        def actualValue = "Actual Value"
+        def actualResult = actualValue ?: "Default Value"
+        println "'Actual Value' ?: 'default': $actualResult"
     }
 
+    /**
+     * This method demonstrates the use of the safe navigation operator (?.) in groovy.
+     * This operator allows you to safely access properties or methods of an object that might be null
+     * without throwing a NullPointerException.
+     */
     static void safeNavigationOperator() {
+        // Here we try to access the 'name' property of a potentially null object
+        // If 'person' is null, it will return null instead of throwing an exception
+        def person = null
+        def name = person?.name
+        println "Safe navigation operator result: $name"
+
+        // If 'person' is not null, it will return the value of 'name'
+        person = [name: "Brendan", role: "Developer"]
+        name = person?.name
+        println "Safe navigation operator result: $name"
+
+        // You can also chain safe navigation operators to navigate through multiple levels
+        def role = person?.role?.toUpperCase()
+        println "Role in uppercase: $role"
     }
 
+
+    /**
+     * 
+     */
     static void ranges() {
     }
 
-
+    /**
+     * 
+     */
     static void truthiness() {
     }
 
+    /**
+     * 
+     */
     static void instanceAndTypeChecking() {
     }
 
