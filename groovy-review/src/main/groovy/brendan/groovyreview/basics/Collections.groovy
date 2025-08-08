9021a6f5855f0d0fbd90f30f14b1ca85d2e03b5f
Brendan Dileo <dileob23@gmail.com>
2025-08-08 18:47:50 -0400

    Update groovy collections review

--- groovy-review/src/main/groovy/brendan/groovyreview/basics/Collections.groovy
+++ groovy-review/src/main/groovy/brendan/groovyreview/basics/Collections.groovy
@@ -1,6 +1,34 @@
 package brendan.groovyreview.basics
 
+import java.util.Collection
+/**
+ *
+ */
 class Collections {
+
+
+
+    static void main(String[] args) {
+
+
+        println "=========== Collection Class ===========\n"
+
+        // Creates a list but can define reference type as Collection
+        // since List implements Collection
+        Collection<String> collection = ["Groovy", "Java", "Kotlin"]
+        println "Collection: $collection"
+        println "Collection class: ${collection.class}"
+
+        // Iterate over the collection
+        collection.each { item ->
+            println "Item: $item"
+        }
+
+        // Shorthand for iteration
+        collection.each { println "Item: $it" }
+
+
+    }
 
 
     
